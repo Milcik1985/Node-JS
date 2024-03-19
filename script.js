87fc1884d738a34045c33e8f8f420dc1b47fe458
Milcik1985 <johndoe@example.commilcik@gmail.com>
2024-03-19 10:23:19 +0200

    task completed

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ const names = ["Onute", "Maryte", "Petras", "Jonas", "Albinas", "Bronius"];
 const sortedNames = names.sort()
 console.log(sortedNames)
 
-const showSortedNames = document.getElementsByClassName("names")[0]
-
 const { v4: uuidv4 } = require("uuid");
 
  const namesWithId = names.map(name => ({name, uuid: uuidv4()}))
- showSortedNames.innerHTML = namesWithId.map(obj => `${obj.name}: ${obj.uuid}`).join("<br>")
-
  console.log(namesWithId);
 
 //  Apsirašyt funkciją kuriai reikia paduot vartotojo vardą bei darbovietę. Funkcija turi gražint vartotojo objektą su vartotojo vardu, darboviete bei user'io id;
